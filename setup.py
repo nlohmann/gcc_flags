1b025d495f580e3eb9f1fa8509dd88c1bc234221
Niels Lohmann <mail@nlohmann.me>
2022-06-18 22:36:48 +0200

    :package: package tool

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup
-import pkg_resources
-import pathlib
-
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
 
 setup(name='gcc_flags',
       version='0.1.0',
@@ -15,6 +6,6 @@ setup(name='gcc_flags',
       url='https://github.com/nlohmann/gcc_flags',
       packages=['gcc_flags'],
       scripts=['gcc_flags/gcc_flags'],
-      install_requires=install_requires,
+      install_requires=['termcolor'],
       include_package_data=True,
       zip_safe=False)
