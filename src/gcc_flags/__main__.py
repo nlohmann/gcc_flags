a6cc9e1040dbb2e734145725dfad58d27cc80565
Niels Lohmann <mail@nlohmann.me>
2022-06-19 13:19:29 +0200

    :hammer: clean up

--- src/gcc_flags/__main__.py
+++ src/gcc_flags/__main__.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
-from gcc_flags import process
+from gcc_flags import process, __version__
 
 def main():
-    parser = argparse.ArgumentParser(description='Collect GCC C++ warning options.')
+    parser = argparse.ArgumentParser(prog='gcc_flags', description='Collect GCC C++ warning options.')
     parser.add_argument('BINARY', help='path to the g++ binary (default: g++)',
                         default='g++', type=str, nargs='?')
+    parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}')
 
     args = parser.parse_args()
 
