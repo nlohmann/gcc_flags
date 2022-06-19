7e69b746d0c78406dbb87822d260e8ba76336d0a
Niels Lohmann <mail@nlohmann.me>
2022-06-19 19:25:00 +0200

    :white_check_mark: add coverage

--- tests/test_module.py
+++ tests/test_module.py
@@ -3,3 +3,11 @@ import gcc_flags
 def test_process():
     res = gcc_flags.process('g++')
     assert len(res) > 0
+
+    s = str(res[0])
+    assert len(s) > 0
+
+    j = dict(res[0])
+    assert 'flag' in j.keys()
+    assert 'help' in j.keys()
+    assert 'error' in j.keys()
